e0fd85d2bf22607dd87b69508c0d8a5f873baa6b
Bartosz Ostafin <ostafinbartek@wp.pl>
2020-01-05 22:49:11 +0100

    Threw some things to functions, added Confidence

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
 #TODO:
-#- Scrap Likes, Overall Grade
-#- Add Confidence Precentage
+#- Scrap Likes, Overall Grade X
+#- Add Confidence Precentage X
 #- Credits System
 #- (Buying Option?)
 #- Exporting to JSON
+#- Show best option = MAX(confidence), MIN(price)
 
 print('Please enter what would you like to look for:')
 userWanted = input()
@@ -17,6 +18,7 @@ webpage = urlopen(url).read()
 web = webpage.decode('utf-8')
 soup = BeautifulSoup(web, 'html.parser')
 
+#FIXME Nie działa kiedy nic nie ma #aboutSeller ex. https://allegro.pl/oferta/steam-f12015-f12017-gtav-rfactor2-project-cars-1-2-8326238065
 def getLikes(given_URL, choice):
     newURL = given_URL + '#aboutSeller'
     url = urlopen(newURL).read()
@@ -30,16 +32,43 @@ def getLikes(given_URL, choice):
             if link.get_text() is not '':
                 return link.get_text()
 
+def calcConfidence(likes,dislikes):
+    likes = int(likes)
+    disLikes = int(dislikes)
+    summed = likes + disLikes
+    confidencePoints = 0
+    if summed >= 10000:
+        confidencePoints += 5
+    elif summed >= 5000:
+        confidencePoints += 4
+    elif summed >= 2000:
+        confidencePoints += 3
+    elif summed >= 1000:
+        confidencePoints += 2
+    elif summed >= 100:
+        confidencePoints += 1
+
+    approvalPrecentage = round(((likes / summed) * 0.05) * 100)
+    confidencePoints += approvalPrecentage
+    return str(confidencePoints)
+
+def getPrice(href):
+    url = urlopen(href).read()
+    soup = BeautifulSoup(url, 'html.parser')
+    for redirect in soup.select('div[aria-label*=cena]'):
+        return redirect.get_text()
+
 for link in soup.select('a[href*=".pl/oferta"]'):
     if link.get_text() is not '':
         title = link.get_text()
         href = link.get('href')
-        print(title)
-        print(href)
-        #FIXME Nie działa kiedy nic nie ma #aboutSeller ex. https://allegro.pl/oferta/steam-f12015-f12017-gtav-rfactor2-project-cars-1-2-8326238065
-        print('Likes: ' + getLikes(href,True))
-        print('Dislikes: ' + getLikes(href,False))
-        url = urlopen(href).read()
-        soup = BeautifulSoup(url, 'html.parser')
-        for redirect in soup.select('div[aria-label*=cena]'):
-            print(redirect.get_text() + '\n')
+        likes = getLikes(href,True)
+        disLikes = getLikes(href, False)
+        price = getPrice(href)
+        print('Title: ' + title)
+        print('URL: ' + href)
+        print('Likes: ' + likes)
+        print('Dislikes: ' + disLikes)
+        print('Price: ' + price)
+        print('Confidence: ' + calcConfidence(likes,disLikes) + '/10')
+        print('\n')
